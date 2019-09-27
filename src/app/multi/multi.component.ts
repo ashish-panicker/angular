fe43211ce9b7b5bff33161f657f234ce7675e04e
ashish-panicker <ashish.s.panicker@gmail.com>
2019-09-27 16:42:13 +0530

    third

--- src/app/multi/multi.component.ts
+++ src/app/multi/multi.component.ts
@@ -7,12 +7,23 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MultiComponent implements OnInit {
 
-  @Input("num1") number1:number;
-  @Input("num2") number2:number;
+  @Input("num1") strNum1:string;
+  @Input("num2") strNum2:string;
+
+  num1:number;
+  num2:number;
+
+  nums:number[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.num2 = parseInt(this.strNum2);
+    this.num1 = parseInt(this.strNum1);
+    
+    for (let index = 1; index <= this.num2; index++) {
+      this.nums.push(index);
+    }
   }
 
 }
